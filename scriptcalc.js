32e3a675bd296be4665a58a063c52a9b17e994d5
Davi Luiz Teixeira Caboclo <daviltcaboclo@gmail.com>
2024-10-29 17:21:08 -0300

    Falta realizar as operações.

---
+++ scriptcalc.js
@@ -0,0 +1,24 @@
+countOp = function() {
+    const x = parseFloat(document.getElementById("N1").value);
+    const y = parseFloat(document.getElementById("N1").value) 
+    let operator; 
+    if (document.getElementById("94").checked);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+cleanInfo = function() {
+    window.location.reload(true);
+}
